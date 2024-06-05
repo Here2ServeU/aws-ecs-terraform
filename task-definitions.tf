ce8fac50d036a936aad050de6ffd85048c77ce87
LearnDevOps <revnaweji@gmail.com>
2024-06-04 22:57:15 -0500

    Create task-definitions.tf

---
+++ task-definitions.tf
@@ -0,0 +1,45 @@
+# Defining the Task Definition for Apache and Nginx Apps.
+
+resource "aws_ecs_task_definition" "apache_task" {
+  family                   = "app1"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name      = "apache-container"
+      image     = "httpd:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_task_definition" "nginx_task" {
+  family                   = "app2-app3"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name      = "nginx-container"
+      image     = "nginx:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+}
